66f7eac7099399f51ad64c741bb88c66a6a722ec
王毣然 <15997610143@163.com>
2024-03-09 10:10:36 +0800

    github主页简历仓库初始化

--- init.sh
+++ init.sh
@@ -2,5 +2,10 @@ git init
 git add .
 git commit -m 'github主页简历仓库初始化'
 
+
+git branch -M master
+ 
 git remote add origin https://github.com/crack-dawn/dawn.github.io.git
 git push -u origin master
+
+ 
