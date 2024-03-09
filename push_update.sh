39513a820cd1aed8319d6ff8b4192dc7180c2567
王毣然 <15997610143@163.com>
2024-03-09 11:29:45 +0800

    normal update

--- push_update.sh
+++ push_update.sh
@@ -1,6 +1,6 @@
 git add .
-git remote rm origin
-git remote add origin https://github.com/crack-dawn/dawn.github.io.git
-git branch -M master
+# git remote rm origin
+# git remote add origin https://github.com/crack-dawn/dawn.github.io.git
+# git branch -M master
 git commit -m "normal update"
 git push -u origin master
---
+++ tempCodeRunnerFile.sh
@@ -0,0 +1,3 @@
+
+# git remote add origin https://github.com/crack-dawn/dawn.github.io.git
+# git branch -M master
