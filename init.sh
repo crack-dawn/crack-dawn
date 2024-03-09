b616f7ef831cd152c1aca9064a4b274de011017a
王毣然 <15997610143@163.com>
2024-03-09 10:08:52 +0800

    github主页简历仓库初始化

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m 'github主页简历仓库初始化'
 
-git remote add origin https://github.com/crack-dawn/dawn.github.io
+git remote add origin https://github.com/crack-dawn/dawn.github.io.git
 git push -u origin master
--- tempCodeRunnerFile.sh
+++
@@ -1,3 +0,0 @@
-
-# git add .
-# git commit -m 'github主页简历仓库初始化'
